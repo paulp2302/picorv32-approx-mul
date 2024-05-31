7dce8682bbd951033b1a2eea6dbfc6b462863a6b
Paul Poelzl <e12022514@student.tuwien.ac.at>
2024-05-31 16:15:05 +0200

    Modify the configurable N-Bit adder implementation to use less resources
    - Fix naming errors in the Makefile

--- HW/ADD/Config/ConfigAddMultiBit.v
+++ HW/ADD/Config/ConfigAddMultiBit.v
@@ -8,21 +8,30 @@ module ConfigAddMultiBit (A, B, Sum);
     input  wire [bitWidth-1:0] B;
     output wire [bitWidth-1:0] Sum;
 
-    wire [bitWidth:0] c_internal; // internal carry
+    wire [approxWidth:0] c_internal; // internal carry
+    wire [bitWidth-1:0] accurateRes;
+    wire [approxWidth-1:0] approxRes;
 
-    genvar j, k;
+    // C_in is always zero in our implementation
+    assign c_internal[0] = 0;
+
+    // Calculate the accurate part of the addition
+    assign accurateRes[bitWidth-1:approxWidth] = A[bitWidth-1:approxWidth] + B[bitWidth-1:approxWidth];
+    assign accurateRes[approxWidth-1:0] = 0;
+
+    // Calculate the total sum from the accurate and appoximate result
+    // The overflow of the approximate adder C_out also needs to be considered
+    assign Sum = accurateRes + approxRes + (c_internal[approxWidth] << approxWidth);
+
+    // Generate multiple instances of the appoximate One-Bit adder to 
+    // create an approximate multiBit adder for approxWidth bits
+    genvar j;
     generate
         // create multiple instances (each ApproxAddOneBit instance performs the addition for one bit position)
         for (j = 0; j < approxWidth; j = j + 1) begin : approx_adders
             // carry output from one bit is fed as the carry input to the next bit
-            ApproxAddOneBit ApproxAddOneBit_Inst (.A(A[j]), .B(B[j]), .Sum(Sum[j]), .Cin(c_internal[j]), .Cout(c_internal[j+1]));
-        end
-        // create multiple instances (each AccurateAddOneBit instance performs the addition for one bit position)
-        for (k = approxWidth; k < bitWidth; k = k + 1) begin : accurate_adders
-        // carry output from one bit is fed as the carry input to the next bit
-            AccurateAddOneBit AccurateAddOneBit_Inst (.A(A[k]), .B(B[k]), .Sum(Sum[k]), .Cin(c_internal[k]), .Cout(c_internal[k+1]));        
+            ApproxAddOneBit ApproxAddOneBit_Inst (.A(A[j]), .B(B[j]), .Sum(approxRes[j]), .Cin(c_internal[j]), .Cout(c_internal[j+1]));
         end
     endgenerate
-
-    assign c_internal[0] = 0;
+    
 endmodule

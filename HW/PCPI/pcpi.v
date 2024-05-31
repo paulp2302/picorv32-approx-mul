8663f4f27654002bbf940b4214cd32c7afba0831
Veronica Kimelman <veronica.kimelman@student.tuwien.ac.at>
2024-05-31 20:26:08 +0200

    Included finished multiplier module

--- HW/PCPI/pcpi.v
+++ HW/PCPI/pcpi.v
@@ -5,29 +5,30 @@ module pcpi(
     output reg pcpi_wr,
     output reg [31:0] pcpi_rd,
     output reg pcpi_wait, pcpi_ready,
-    output reg [SIZE-1:0] state // only for testing, remove later
 );
 
+Config16x16Mul multiplier(.a (a), .b (a), .out (mul_out));
+
 parameter SIZE = 4;
-parameter MUL16 = 7'b0001011;
+parameter MUL16 = 7'b0101011;
 parameter IDLE = 3'b001, DECODE = 3'b010, EXECUTE = 3'b011, DONE = 3'b100;
 
 // Internal declarations
-//reg [SIZE-1:0] state; UNCOMMENT WHEN YOU DONT NEED IT AS OUTPUT
+reg [SIZE-1:0] state; 
 reg [SIZE-1:0] next_state;
+reg [15:0] a,b;
 wire [6:0] opcode;
-wire  mul_waiting;
 
 assign opcode = pcpi_insn[6:0];
 
-
 // Declarations for MUL module
 wire [31:0] mul_out;
-wire mul_start, mul_finish;
+//wire mul_start, mul_finish; // Keep this if we don't make STA
+//reg[15:0] prev_rs1, prev_rs2, prev_out;
 
 // Temporarly assignments, until Mul module is done
-assign mul_finish = 0; 
-assign mul_waiting = !mul_finish;
+//assign mul_finish = 1; 
+
 
 // FSM Sequence
 always @ (posedge clk) begin
@@ -43,10 +44,9 @@ always @ (state or pcpi_valid) begin
     case(state) 
         IDLE:    if (pcpi_valid)
                     next_state = DECODE;
-
         DECODE:  if (opcode == MUL16)
-                    next_state = EXECUTE;
-        EXECUTE: if (mul_finish)
+                    next_state = DONE;
+        EXECUTE: if (mul_finish)        // Keep this state if we don't pass STA, otherwise remove
                     next_state = DONE;
                  else
                     next_state = EXECUTE;
@@ -59,11 +59,14 @@ end
 always @ (state) begin
     pcpi_wr <= 0;
     pcpi_ready <= 0;
-    pcpi_wait <= 0;
+    pcpi_wait <= 0; // Remove this if we make STA, then we can have this set to 0 always
     case(state)
         IDLE: pcpi_ready <= 0;  
-        DECODE: pcpi_ready <= 1;
-        EXECUTE:    pcpi_wait <= 1;
+        DECODE: begin
+                a <= pcpi_rs1[15:0];
+                b <= pcpi_rs2[15:0];
+        end
+        EXECUTE,  
         DONE: begin
                 pcpi_wr <= 1;
                 pcpi_ready <= 1;

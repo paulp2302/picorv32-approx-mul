f146a59b5c828b39d68037964d7b8e7e8333f58e
Paul Poelzl <e12022514@student.tuwien.ac.at>
2024-06-04 10:41:32 +0200

    Fix error when approxWidth is set to zero

--- HW/ADD/Config/ConfigAddMultiBit.v
+++ HW/ADD/Config/ConfigAddMultiBit.v
@@ -1,8 +1,7 @@
 // Configurable Multiple Bit Adder Implementation
 module ConfigAddMultiBit (A, B, Sum);
-
-    parameter bitWidth = 32;
-    parameter approxWidth = 0; 
+    parameter [5:0] bitWidth = 32;
+    parameter [5:0] approxWidth = 0; 
 
     input  wire [bitWidth-1:0] A;
     input  wire [bitWidth-1:0] B;
@@ -20,7 +19,8 @@ module ConfigAddMultiBit (A, B, Sum);
 
     // Calculate the total sum from the accurate and appoximate result
     // The overflow of the approximate adder C_out also needs to be considered
-    assign Sum = accurateRes + approxRes + (c_internal[approxWidth] << approxWidth);
+    assign Sum = (approxWidth == 0) ? accurateRes 
+                    : accurateRes + approxRes + (c_internal[approxWidth] << approxWidth);
 
     // Generate multiple instances of the appoximate One-Bit adder to 
     // create an approximate multiBit adder for approxWidth bits
@@ -28,8 +28,8 @@ module ConfigAddMultiBit (A, B, Sum);
     generate
         // create multiple instances (each ApproxAddOneBit instance performs the addition for one bit position)
         for (j = 0; j < approxWidth; j = j + 1) begin : approx_adders
-            // carry output from one bit is fed as the carry input to the next bit
-            ApproxAddOneBit ApproxAddOneBit_Inst (.A(A[j]), .B(B[j]), .Sum(approxRes[j]), .Cin(c_internal[j]), .Cout(c_internal[j+1]));
+        // carry output from one bit is fed as the carry input to the next bit
+        ApproxAddOneBit ApproxAddOneBit_Inst (.A(A[j]), .B(B[j]), .Sum(approxRes[j]), .Cin(c_internal[j]), .Cout(c_internal[j+1]));
         end
     endgenerate
     

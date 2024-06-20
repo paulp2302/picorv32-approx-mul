8b71b9c24c6d726f89201b4c81fec57283b7a137
Veronica Kimelman <veronica.kimelman@student.tuwien.ac.at>
2024-06-20 10:23:19 +0200

    Added licsencing and removed non-working files

--- HW/MUL/2x2/testbench.v
+++ HW/MUL/2x2/testbench.v
@@ -1,3 +1,10 @@
+// This is free and unencumbered software released into the public domain.
+//
+// Anyone is free to copy, modify, publish, use, compile, sell, or
+// distribute this software, either in source code form or as a compiled
+// binary, for any purpose, commercial or non-commercial, and by any
+// means.
+
 // Universal testbench for the 2x2 multipliers 
 
 `timescale 100ps/10ps // Unit of time is 100ps
